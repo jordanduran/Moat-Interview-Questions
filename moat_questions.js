3f47c164b3844f71e484134a853c6062377894b4
Jordan Duran <jordanduran@static-192-165-1-12.cust.crystone.se>
2020-06-06 17:07:53 -0400

    arraySum Function

--- moat_questions.js
+++ moat_questions.js
@@ -11,3 +11,25 @@ function getFileExtension(file) {
 
   return words[words.length - 1];
 }
+
+function arraySum(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return false;
+  }
+
+  let total = 0;
+  let size = array.length;
+  let i = 0;
+
+  for (i = 0; i < size; i++) {
+    if (typeof array[i] == 'number') {
+      total = total + array[i];
+    }
+
+    if (Array.isArray(array[i])) {
+      total = total + arraySum(array[i]);
+    }
+  }
+
+  return total;
+}
